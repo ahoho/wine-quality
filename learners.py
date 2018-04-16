602aa7036cde568e8ef7ba5c26f0cd66cb50dd09
ahoho <ahoyle@wesleyan.edu>
2018-04-16 11:01:08 +0100

    create evaluation function

--- learners.py
+++ learners.py
@@ -3,6 +3,7 @@ import pickle
 import feather
 import scipy
 import numpy as np
+import pandas as pd
 
 from sklearn.dummy import DummyRegressor, DummyClassifier
 from sklearn.linear_model import LinearRegression, LogisticRegression
@@ -15,6 +16,8 @@ from sklearn.model_selection import (
 from sklearn.metrics import (
   mean_squared_error, mean_absolute_error, f1_score, make_scorer)
 from sklearn.utils import resample
+
+TRAIN_MODELS = False # set to true to train our models
 
 def gen_data(data_fpath, test_size=0.2, random_state=1234, features_to_drop=[]):
   """
@@ -141,103 +144,165 @@ def train(model_dir, x_train, y_train):
       with open('./intermediate/{}/clf_{}.pkl'.format(model_dir, name), 'wb') as o:
           pickle.dump(clf, o)
 
-  def load_models(model_dir):
-    """
-    Load saved models
-    """
-    model_names = ['dummy', 'linear', 'svm', 'rf', 'mlp']
-    regressors, classifiers = {}, {}
+def load_models(model_dir):
+  """
+  Load saved models
+  """
+  model_names = ['dummy', 'linear', 'svm', 'rf', 'mlp']
+  regressors, classifiers = {}, {}
 
-    for name in model_names:
-      with open('./intermediate/{}/reg_{}.pkl'.format(model_dir, name), 'rb') as i:
-        regressors[name] = pickle.load(i)
-      with open('./intermediate/{}/clf_{}.pkl'.format(model_dir, name), 'rb') as i:
-        classifiers[name] = pickle.load(i)
+  for name in model_names:
+    with open('./intermediate/{}/reg_{}.pkl'.format(model_dir, name), 'rb') as i:
+      regressors[name] = pickle.load(i)
+    with open('./intermediate/{}/clf_{}.pkl'.format(model_dir, name), 'rb') as i:
+      classifiers[name] = pickle.load(i)
 
-    return regressors, classifiers
+  return regressors, classifiers
 
-  def evaluate_models(model_dir, metric, x_test, y_test, **kwargs):
-    """
-    Evaluate the models and save the results
-    """
-    all_models = load_models(model_dir)
-    results = []
-    # iterate through models
-    for model_type, models in zip(('reg', 'clf'), all_models):
-      
-      for model_name, model in models.items():
+def test_err_cv(model, metric, test_x, test_y, n_splits=3, seed=1234):
+  """
+  Calculate mean and confidence intervals for a metric on a cross-validated
+  dataset
+  N.B.: metric should be point-wise
+  """
+  # unsure whether to use stratified k-fold or regular k-fold?
+  skf = StratifiedKFold(n_splits=3)
+  errors = np.array([])
+
+  for idx, _ in skf.split(test_x, test_y):
+    test_x_fold, test_y_fold = test_x[idx], text_y[idx]
+    pred_y_fold = model.predict(test_x_fold)
+    error = metric(test_y_fold, pred_y_fold)
+    errors = np.concatenate([errors, error])
+
+  mean = np.mean(errors)
+  # TODO: Complete this function
+  pass
+
+def test_err_bootstrap(model, metric, x_test, y_test,
+                        n_iter=1000, alpha=0.05, seed=1234):
+  """
+  Calculate mean and 100*(1-alpha)% central confidence intervals for a metric
+  on a bootstrapped dataset
+  """
+  np.random.seed(seed)
+  results = []
+
+  for _ in range(n_iter):
+    # by default, samples the same number in array, with replacement
+    x_test_bs, y_test_bs = resample(x_test, y_test)
+    y_pred_bs = model.predict(x_test_bs)
+    results.append(metric(y_test_bs, y_pred_bs))
+    print(i, end='\r', flush=True)
+  
+  a = 100 * alpha / 2
+  lb, ub = np.percentile(results, (a, 100 - a))
+  mean = np.mean(results)
+
+  return mean, lb, ub
+
+def evaluate_models(model_dir, x_test, y_test, **kwargs):
+  """
+  Evaluate the models and save the results
+  """
+  all_models = load_models(model_dir)
+
+  results = []
+  metrics = {
+    'reg': [
+      ('mse', mean_squared_error),
+      ('mae', mean_absolute_error),
+    ],
+    'clf': [
+      ('mse', mean_squared_error),
+      ('mae', mean_absolute_error),
+      ('f1_score', 
+        lambda y_true, y_pred: f1_score(
+          y_true, y_pred, labels=[3,4,5,6,7,8,9], average='micro'
+        )
+      )
+    ]
+  }
+
+  # iterate through models
+  for model_type, model_set in zip(('reg', 'clf'), all_models):
+    for model_name, model in model_set.items():
+      for metric_name, metric in metrics[model_type]:
+        print('On {} model {}, calculating {}'.format(
+          model_type, model_name, metric_name), end='\r', flush=True)
+        
         mean, lb, ub = test_err_bootstrap(model, metric, x_test, y_test, **kwargs)
         results.append({
           'type': model_type,
           'model': model_name,
+          'metric': metric_name,
           'mean': mean,
           'lb': lb,
-          'ub': ub}
-        )
+          'ub': ub
+        })
 
-    return results
+  results = pd.DataFrame(results, 
+    columns=['type', 'model', 'metric', 'mean', 'lb', 'ub'])
 
-  def test_err_cv(model, metric, test_x, test_y, n_splits=3, seed=1234):
-    """
-    Calculate mean and confidence intervals for a metric on a cross-validated
-    dataset
-    N.B.: metric should be point-wise
-    """
-    # unsure whether to use stratified k-fold or regular k-fold?
-    skf = StratifiedKFold(n_splits=3)
-    errors = np.array([])
+  return results
 
-    for idx, _ in skf.split(test_x, test_y):
-      test_x_fold, test_y_fold = test_x[idx], text_y[idx]
-      pred_y_fold = model.predict(test_x_fold)
-      error = metric(test_y_fold, pred_y_fold)
-      errors = np.concatenate([errors, error])
+def rf_feature_importances(model, labels):
+  """
+  Return feature importances for a random forest model
+  """
+  rf = model.best_estimator_
 
-    mean = np.mean(errors)
-    # TODO:
+  # get mean importances and standard deviation
+  importances_mean = rf.feature_importances_
+  importances_sd = np.std(
+    [tree.feature_importances_ for tree in rf.estimators_], axis=0)
+  
+  # label and return
+  return pd.DataFrame(
+    {'feature': labels, 'mean': importances_mean, 'sd': importances_sd})
 
-
-  def test_err_bootstrap(model, metric, x_test, y_test,
-                         n_iter=1000, alpha=0.05, seed=1234):
-    """
-    Calculate mean and 100*(1-alpha)% central confidence intervals for a metric
-    on a bootstrapped dataset
-    """
-    np.random.seed(seed)
-    results = []
-
-    for _ in range(n_iter):
-      # by default, samples the same number in array, with replacement
-      x_test_bs, y_test_bs = resample(x_test, y_test)
-      y_pred_bs = model.predict(x_test_bs)
-      results.append(metric(y_test_bs, y_pred_bs))
-    
-    a = 100 * alpha / 2
-    lb, ub = np.percentile(results, (a, 100 - a))
-    mean = np.mean(results)
-
-    return mean, lb, ub
 
 if __name__ == '__main__':
   data_fpath = './intermediate/wine_logged_unscaled.feather'
   
-  # train on all data
+  ## Data Collection 
+  # all functions below maintain same train-test split
+  # all data
   x_train, x_test, y_train, y_test = gen_data(data_fpath)
-  #train('unscaled', x_train, y_train)
-
-  # train on colors only
+  # color only
   x_train_col, x_test_col, y_train_col, y_test_col = gen_data(
     data_fpath, features_to_drop=[c for c in x_train.columns if c != 'color'])
-  train('unscaled-color', x_train, y_train)
-
-  # train on chemicals, no color
+  # chemicals only
   x_train_chm, x_test_chm, y_train_chm, y_test_chm = gen_data(
     data_fpath, features_to_drop=['color'])
-  train('unscaled-chemical', x_train, y_train)
 
+  ## Training
+  if TRAIN_MODELS:
+    # train on all data
+    #train('unscaled', x_train, y_train)
+
+    # train on colors only
+    train('unscaled-color', x_train_col, y_train_col)
+
+    # train on chemicals, no color
+    train('unscaled-chemical', x_train_chm, y_train_chm)
+
+  ## Evaluation
+  results_all = evaluate_models('unscaled', x_test, y_test)
+  results_col = evaluate_models('unscaled-color', x_test_col, y_test_col)
+  results_chm = evaluate_models('unscaled-chemical', x_test_chm, y_test_chm)
+
+  #save 
+  results_all.to_csv('./output/results_unscaled_all.csv', index=False)
+  results_col.to_csv('./output/results_color_all.csv', index=False)
+  results_chm.to_csv('./output/results_chemical_all.csv', index=False)
+
+  ## Model-specific results
+  regressors, classifiers = load_models('unscaled')
+  # get rf importances
+  rf_importances = rf_feature_importances(regressors['rf'], x_train.columns)
+  rf_importances.to_csv('./output/rf_importances_all.csv', index=False)
 
   # TODO:
   # finish variance reduction/kfold
-  # evaluate with chemicals/colors
-  # determine most important tree features
-  # determine linear regression coefficients
+  # determine linear regression coefficients (likely preerable to do in R)
